b16a8d46137cc96bf88564e70f7743eb19d637a3
Alan01777 <camp.alan3@gmail.com>
2022-10-13 13:28:50 -0300

    php test

--- action.php
+++ action.php
@@ -1,3 +1,13 @@
 <?php
 
-echo "Funciona";
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$matricula = $_POST['matricula'];
+$senha = $_POST['senha'];
+$curso = $_POST['curso'];
+
+echo "Seu nome é: $nome";
+echo "Seu email é: $email";
+echo "Sua senha é: $senha";
+echo "Sua matricula é: $matricula";
+echo "Seu curso é: $curso";
