21c36d03bcd1fb31c82a10d0fd6a81521014c10e
Alan01777 <camp.alan3@gmail.com>
2022-10-13 13:34:30 -0300

    php test

--- action.php
+++ action.php
@@ -6,8 +6,8 @@ $matricula = $_POST['matricula'];
 $senha = $_POST['senha'];
 $curso = $_POST['curso'];
 
-echo "Seu nome é: $nome\n";
-echo "Seu email é: $email\n";
-echo "Sua senha é: $senha\n";
-echo "Sua matricula é: $matricula\n";
-echo "Seu curso é: $curso\n";
+echo "Seu nome é: $nome<br>";
+echo "Seu email é: $email<br>";
+echo "Sua senha é: $senha<br>";
+echo "Sua matricula é: $matricula<br>";
+echo "Seu curso é: $curso<br>";
