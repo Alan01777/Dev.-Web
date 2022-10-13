2c0c41c45d0ed839cff7b9b98af5c25072d34044
Alan01777 <camp.alan3@gmail.com>
2022-10-13 14:00:32 -0300

    php teste

--- action.php
+++ action.php
@@ -5,9 +5,15 @@ $email = $_POST['email'];
 $matricula = $_POST['matricula'];
 $senha = $_POST['senha'];
 $curso = $_POST['curso'];
+$data = $_POST['data-nascimento'];
+$aplicacao = $_POST['are-aplicacao'];
+
+
 
 echo "Seu nome é: $nome<br>";
 echo "Seu email é: $email<br>";
 echo "Sua senha é: $senha<br>";
 echo "Sua matricula é: $matricula<br>";
 echo "Seu curso é: $curso<br>";
+echo "Sua data de nascimento é: $data<br>";
+echo "Sua area de aplicação é: $area<br>";
